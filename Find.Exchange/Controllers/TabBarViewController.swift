befc26aee384297413f49fb3b810bcae0a5b068d
S Tunca <sezertunca@gmail.com>
2018-10-19 00:34:16 +0100

    Add tab bar images, add all dummy view controllers to the tabbar vc

--- Find.Exchange/Controllers/TabBarViewController.swift
+++ Find.Exchange/Controllers/TabBarViewController.swift
@@ -17,8 +17,35 @@ class TabBarViewController : UITabBarController
         
         let layout = UICollectionViewFlowLayout()
         let moneyTabCollectionViewController = MoneyTabCollectionViewController(collectionViewLayout: layout)
-        let navController = UINavigationController(rootViewController: moneyTabCollectionViewController)
+        let marketTabNavController = UINavigationController(rootViewController: moneyTabCollectionViewController)
         
-        viewControllers = [navController]
+        // withRenderingMode always original preserves the original colour
+        marketTabNavController.tabBarItem.image = UIImage(named: "market")?.withRenderingMode(.alwaysOriginal)
+        marketTabNavController.tabBarItem.selectedImage = UIImage(named: "market_active")?.withRenderingMode(.alwaysOriginal)
+        
+        let accountTabViewController = UIViewController()
+        accountTabViewController.tabBarItem.image = UIImage(named: "accounts")?.withRenderingMode(.alwaysOriginal)
+        accountTabViewController.tabBarItem.selectedImage = UIImage(named: "accounts")?.withRenderingMode(.alwaysTemplate)
+        
+        let cardTabViewController = UIViewController()
+        cardTabViewController.tabBarItem.image = UIImage(named: "card")?.withRenderingMode(.alwaysOriginal)
+        cardTabViewController.tabBarItem.selectedImage = UIImage(named: "card")?.withRenderingMode(.alwaysTemplate)
+        
+        let qrTabViewController = UIViewController()
+        qrTabViewController.tabBarItem.image = UIImage(named: "qr")?.withRenderingMode(.alwaysOriginal)
+        qrTabViewController.tabBarItem.selectedImage = UIImage(named: "qr")?.withRenderingMode(.alwaysOriginal)
+        
+        let moreTabViewController = UIViewController()
+        moreTabViewController.tabBarItem.image = UIImage(named: "more")?.withRenderingMode(.alwaysOriginal)
+        moreTabViewController.tabBarItem.selectedImage = UIImage(named: "more")?.withRenderingMode(.alwaysTemplate)
+        
+        viewControllers =
+        [
+            accountTabViewController,
+            cardTabViewController,
+            qrTabViewController,
+            marketTabNavController,
+            moreTabViewController
+        ]
     }
 }
